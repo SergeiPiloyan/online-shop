abb93f6694182d6c090c335f2093d367fed2d6d5
SergeiPiloyan <sergei.piloyan0302@gmail.com>
2023-11-10 13:55:34 +0200

    added and described modules

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
-
+const models = require('./models/models')
 
 const PORT = process.env.PORT || 5000;
-
 const app = express()
+
 
 const start = async () => {
     try {
