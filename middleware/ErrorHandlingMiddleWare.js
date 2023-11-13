f73f8da3d6ac01508bf7bb3182009119952da053
SergeiPiloyan <sergei.piloyan0302@gmail.com>
2023-11-13 19:46:38 +0200

    added error handler

---
+++ middleware/ErrorHandlingMiddleWare.js
@@ -0,0 +1,10 @@
+const apiError = require('../error/ApiError')
+
+module.exports = function (err, req, res, nest) {
+
+    if (err instanceof apiError) {
+        return res.status(err.status).json({ message: err.message })
+    }
+
+    return res.status(500).json({ message: "Непредвиденная ошибка" })
+}
