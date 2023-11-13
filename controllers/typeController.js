0eff06c3cea1b5ac210c62df6f2ada359e882543
SergeiPiloyan <sergei.piloyan0302@gmail.com>
2023-11-13 20:56:57 +0200

    updated brand and type controllers

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -1,8 +1,20 @@
+const { Type } = require('../models/models')
+const apiError = require('../error/ApiError')
+
 class TypeController {
 
-    async create(req, res) { }
+    async create(req, res) {
+        const { name } = req.body;
+        const type = await Type.create({ name })
+        res.json(type)
+    }
 
-    async getAll(req, res) { }
+    async getAll(req, res) {
+        const types = await Type.findAll()
+        return res.json(types)
+    }
+
+    async delete(req, res) { }
 
 }
 
