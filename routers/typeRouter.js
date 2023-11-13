20cfe9d05d5ea96e2289e73ca19372249c0d79a1
SergeiPiloyan <sergei.piloyan0302@gmail.com>
2023-11-13 20:11:20 +0200

    updated routers

--- routers/typeRouter.js
+++ routers/typeRouter.js
@@ -2,9 +2,9 @@ const Router = require('express')
 const router = new Router()
 const typeController = require('../controllers/typeController')
 
-router.post('/',)
-router.get('/')
-router.delete('/')
+router.post('/', typeController.create)
+router.get('/', typeController.getAll)
+router.delete('/', typeController.delete)
 
 
 
