49956bc7db96d574784f431421ba99036ddef045
SergeiPiloyan <sergei.piloyan0302@gmail.com>
2023-11-13 19:11:10 +0200

    added routers and controllers

---
+++ routers/brandRouter.js
@@ -0,0 +1,10 @@
+const Router = require('express')
+const router = new Router()
+const brandController = require('../controllers/brandController')
+
+router.post('/',)
+router.get('/')
+router.delete('/')
+
+
+module.exports = router;
---
+++ routers/typeRouter.js
@@ -0,0 +1,11 @@
+const Router = require('express')
+const router = new Router()
+const typeController = require('../controllers/typeController')
+
+router.post('/',)
+router.get('/')
+router.delete('/')
+
+
+
+module.exports = router;
