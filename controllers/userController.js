c1ceeadfe4f1654fa0a038f5c6cc798a81d2b53b
SergeiPiloyan <sergei.piloyan0302@gmail.com>
2023-11-13 19:59:52 +0200

    updated userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,20 @@
+const apiError = require('../error/ApiError')
+
 class UserController {
 
     async registration(req, res) { }
 
     async login(req, res) { }
 
-    async check(req, res) { }
+    async check(req, res, next) {
+        const { id } = req.query
+
+        if (!id) {
+            return next(apiError.badRequest(`id isn't specified`))
+        }
+
+        return res.json(id)
+    }
 }
 
 
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const sequelize = require('./db')
 const cors = require('cors')
 const models = require('./models/models')
 const router = require('./routers/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleWare')
+
 
 const PORT = process.env.PORT || 5000;
 const app = express()
@@ -12,9 +14,8 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'WORKING' })
-})
+// обработка ошибок, последний MiddleWare
+app.use(errorHandler)
 
 const start = async () => {
     try {
