0677a909673d70657624570846a88012842a0bfa
SergeiPiloyan <sergei.piloyan0302@gmail.com>
2023-11-15 14:40:09 +0200

    updated devicce controller

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const fileUpload = require('express-fileupload')
 const models = require('./models/models')
 const router = require('./routers/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleWare')
-
+const path = require('path')
 
 const PORT = process.env.PORT || 5000;
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
